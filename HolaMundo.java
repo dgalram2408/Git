a568da5a09cabc5fc8145df3cf8d6a1530b7ca75
David <dgalram2408@g.educaand.es>
2024-10-25 20:24:09 +0200

    nueva rama con segundoprograma

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,10 +2,8 @@ public class HolaMundo {
 	public static void main(String[] args){
 		int num1=5, num2=3;
 		System.out.println("Hola mundo");
-<<<<<<< HEAD
-		
-=======
+
 		System.out.println("La suma de los números es "+(num1+num2));
->>>>>>> feature-suma
+
 	}
 }
